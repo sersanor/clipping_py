3cb1653b16dc6df9d2f041996b5bfb4fa5baacb3
Sergio Sanchez Ortiz <sersanor@gmail.com>
2021-03-08 01:05:14 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+#CLIPPING EXAMPLE
+
+#Name of the Book
+#- Data for the clipping position page, position, time
+#
+#clipping text
+#==========
+
+import csv
+
+inputFile = "clip.txt"
+outputFile = "sal.csv"
+
+fr = open(inputFile, 'r')
+fw = open(outputFile, 'w')
+writer = csv.writer(fw, delimiter=",")
+writer.writerow(["Book","Location","Text","Hash"])
+auxList = []
+count = 0
+for i in fr:
+    if count == 0:
+        auxList.append(i)
+    if count == 1:
+        auxList.append(i)
+    if count == 3:
+        auxList.append(i)
+    if count == 4:
+        #Generate Hash from List
+        auxList.insert(0,hash(str(auxList)))
+        writer.writerow(auxList)
+        auxList = []
+        count = 0
+        continue
+    count+=1
+
+fr.close()
+fw.close()
